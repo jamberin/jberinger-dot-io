0f854e4073f1ec415a15d50857919a982914a5ce
James Beringer <jamberin@gmail.com>
2023-12-20 13:58:13 -0500

    Initial commit with personal details

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - undefined v0.0.1 (undefined)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/jberinger-dot-io/blob/master/LICENSE)
 */
 //
 // Scripts
